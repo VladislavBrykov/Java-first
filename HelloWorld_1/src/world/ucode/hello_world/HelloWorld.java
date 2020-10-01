951f57fc99a920546c86b9039d9c7a100d94823e
VladislavBrykov <vlad5brykov@gmail.com>
2020-10-01 23:56:26 +0300

    first program

---
+++ HelloWorld_1/src/world/ucode/hello_world/HelloWorld.java
@@ -0,0 +1,8 @@
+package world.ucode.hello_world;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println(str);
+    }
+}
