f2fbd8e814339aa8217419e76b3e77583793d5df
VladislavBrykov <vlad5brykov@gmail.com>
2020-10-05 23:40:38 +0300

    Wrapper

---
+++ Wrapper/src/world/ucode/_wrapper/Wrapper.java
@@ -0,0 +1,10 @@
+package world.ucode._wrapper;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        int i= 345;
+        Integer I = i;
+        String str = I.toString();
+        System.out.println(str);
+    }
+}
