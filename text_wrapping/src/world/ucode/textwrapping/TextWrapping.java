9b618da6eace6eff112a3acad15cf0551c18a1f8
VladislavBrykov <vlad5brykov@gmail.com>
2020-10-05 21:30:44 +0300

    text_wrapping

---
+++ text_wrapping/src/world/ucode/textwrapping/TextWrapping.java
@@ -0,0 +1,9 @@
+package world.ucode.textwrapping;
+
+public class TextWrapping {
+    public static void main(String[] args) {
+        System.out.print("Hello\n" +
+                "Vlad\n" +
+                "Your salary is $ 10,000");
+    }
+}
